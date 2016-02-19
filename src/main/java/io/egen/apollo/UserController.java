f80d7aab3c7718f1deae32dcf173bbb7935f998e
SHIVA MARISWAMY SUBRAMANI <shivamar@buffalo.edu>
2016-02-19 05:54:37 -0800

    chore(setup): project setup

---
+++ src/main/java/io/egen/apollo/UserController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package io.egen.apollo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Shiva
+ *
+ */
+
+@RestController
+public class UserController {
+
+}
